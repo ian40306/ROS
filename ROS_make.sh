bdbdb18141988725d4fa648bebf95c6f63469b63
ian40306 <43855922+ian40306@users.noreply.github.com>
2019-09-05 07:48:02 +0800

    Update ROS_make.sh

--- ROS_make.sh
+++ ROS_make.sh
@@ -1,8 +1,6 @@
 apt-get update
 apt-get install iputils-ping -y
 apt-get install vim -y
-apt update
-apt install git -y
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
