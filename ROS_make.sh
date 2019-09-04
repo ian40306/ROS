925ac481678d4d2342d9577a30bfcc977b3927a8
ian40306 <43855922+ian40306@users.noreply.github.com>
2019-09-04 21:59:12 +0800

    Create ROS_make.sh

---
+++ ROS_make.sh
@@ -0,0 +1,15 @@
+apt-get update
+apt-get install iputils-ping -y
+apt-get install vim -y
+apt update
+apt install git -y
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full -y
+apt-cache search ros-kinetic
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
