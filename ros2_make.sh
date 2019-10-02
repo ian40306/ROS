38b5984fe6059f31b890e71faaf393d1ef8fe0af
ian40306 <43855922+ian40306@users.noreply.github.com>
2019-10-02 14:25:47 +0800

    Update ros2_make.sh

--- ros2_make.sh
+++ ros2_make.sh
@@ -4,6 +4,7 @@ pip3 install --upgrade pip
 apt-get update
 apt-get install wget
 sudo apt update && sudo apt install curl gnupg2 lsb-release
+y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
 wget https://github.com/ros2/ros2/releases/download/release-crystal-20190820/ros2-crystal-20190820-linux-bionic-amd64.tar.bz2
