8dfcb54f334cfd8cc459c119daa97c739915ebc3
ian40306 <43855922+ian40306@users.noreply.github.com>
2019-10-13 13:55:09 +0800

    Update ros2_make.sh

--- ros2_make.sh
+++ ros2_make.sh
@@ -25,6 +25,13 @@
 #docker run --name 0858804-1 -p 6080:80 -p 5900:5900 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc:bionic
 #apt update
 #apt install git -y
+sudo apt-get update
+sudo apt-get install wget
+wget https://github.com/ros2/ros2/releases/download/release-crystal-20190820/ros2-crystal-20190820-linux-bionic-amd64.tar.bz2
+mkdir -p ~/ros2_install
+cd ~/ros2_install
+tar xf ~/ROS/ros2-crystal-20190820-linux-bionic-amd64.tar.bz2
+cd
 export LANG=en_US.UTF-8
 sudo apt update && sudo apt install curl gnupg2 lsb-release -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
