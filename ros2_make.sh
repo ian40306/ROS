74f91bf63a2f3b77d7f5d488a7865ed2af36f5ff
ian40306 <43855922+ian40306@users.noreply.github.com>
2019-10-13 23:00:01 +0800

    Add files via upload

--- ros2_make.sh
+++ ros2_make.sh
@@ -1,45 +1,60 @@
 #!/bin/bash
-#test
-#sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 150
-#apt update
-#apt install python3-pip -y
-#pip3 install --upgrade pip
-#apt-get update
-#apt-get install wget
-#sudo apt update && sudo apt install curl gnupg2 lsb-release
-#y
-#curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-#sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-#wget https://github.com/ros2/ros2/releases/download/release-crystal-20190820/ros2-crystal-20190820-linux-bionic-amd64.tar.bz2
-#mkdir -p ~/ros2_install
-#cd ~/ros2_install
-#tar xf ~/ROS/ros2-crystal-20190820-linux-bionic-amd64.tar.bz2
-#sudo apt install -y python-rosdep
-#rosdep init
-#rosdep update
-#CHOOSE_ROS_DISTRO=crystal
-#rosdep install --from-paths ros2-linux/share --ignore-src --rosdistro $CHOOSE_ROS_DISTRO -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 libopensplice69 osrf_testing_tools_cpp poco_vendor rmw_connext_cpp rosidl_typesupport_connext_c rosidl_typesupport_connext_cpp rti-connext-dds-5.3.1 tinyxml_vendor tinyxml2_vendor urdfdom urdfdom_headers"
-
-
-#install ros2 by ubuntu 18.04 with docker
-#docker run --name 0858804-1 -p 6080:80 -p 5900:5900 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc:bionic
-#apt update
-#apt install git -y
-sudo apt-get update
-sudo apt-get install wget
-wget https://github.com/ros2/ros2/releases/download/release-crystal-20190820/ros2-crystal-20190820-linux-bionic-amd64.tar.bz2
-mkdir -p ~/ros2_install
-cd ~/ros2_install
-tar xf ~/ROS/ros2-crystal-20190820-linux-bionic-amd64.tar.bz2
-cd
+#make ros2 for ubuntu18.04 desktop & install example like turtlesim
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
-sudo apt update && sudo apt install curl gnupg2 lsb-release -y
+sudo apt update && sudo apt install curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-export CHOOSE_ROS_DISTRO=crystal
 sudo apt update
-sudo apt install ros-$CHOOSE_ROS_DISTRO-desktop -y
-sudo apt install ros-$CHOOSE_ROS_DISTRO-ros-base
+sudo apt install ros-dashing-desktop
 sudo apt install python3-argcomplete
-source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash
-echo "source /opt/ros/$CHOOSE_ROS_DISTRO/setup.bash" >> ~/.bashrc
+source /opt/ros/dashing/setup.bash
+echo "source /opt/ros/dashing/setup.bash" >> ~/.bashrc
+sudo apt update && sudo apt install -y \
+  build-essential \
+  cmake \
+  git \
+  python3-colcon-common-extensions \
+  python3-lark-parser \
+  python3-pip \
+  python-rosdep \
+  python3-vcstool \
+  wget
+# install some pip packages needed for testing
+python3 -m pip install -U \
+  argcomplete \
+  flake8 \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest \
+  pytest-cov \
+  pytest-runner \
+  setuptools
+# install Fast-RTPS dependencies
+sudo apt install --no-install-recommends -y \
+  libasio-dev \
+  libtinyxml2-dev
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro dashing -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 libopensplice69 rti-connext-dds-5.3.1 urdfdom_headers"
+python3 -m pip install -U lark-parser
+wget https://github.com/ros2/ros2/releases/download/release-dashing-20190910/ros2-dashing-20190910-linux-bionic-amd64.tar.bz2 
+mkdir -p ~/ros2_install && cd ~/ros2_install
+tar xf ~/ros2-dashing-20190910-linux-bionic-amd64.tar.bz2
+sudo apt-get update && sudo apt-get install vim -y
+echo ". ~/ros2_install/ros2-linux/setup.bash" >> ~/.bashrc
+mkdir -p ros2_turtlesim_example/src && cd ros2_turtlesim_example/src
+git clone https://github.com/zhangrelay/ros2_tutorials.git
+cd ..
+colcon build
+echo "source ~/ros2_turtlesim_example/install/local_setup.bash" >> ~/.bashrc
+echo "source ~/ros2_turtlesim_example/install/setup.bash" >> ~/.bashrc
